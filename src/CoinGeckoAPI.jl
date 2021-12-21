267ea237995d439469352339e3e942a0f34cfb16
Gil Junqueira <gjunqueira@msn.com>
2021-12-21 14:39:05 -0600

    added ping function

--- src/CoinGeckoAPI.jl
+++ src/CoinGeckoAPI.jl
@@ -1,5 +1,33 @@
 module CoinGeckoAPI
 
-# Write your package code here.
+# dependacies
+
+using HTTP
+using JSON3
+
+
+# globals constants
+const global url_base = "https://api.coingecko.com/api/v3/"
+
+
+
+
+"""
+    ping()
+Function to check if the API is up and running
+
+# Arguments
+    None
+
+# Returns
+"""
+function ping()
+    apiurl ="ping"
+    r =  HTTP.request("GET", url_base * apiurl)
+    r = String(r.body)
+    return JSON3.read(r)
+
+end
+
 
 end
