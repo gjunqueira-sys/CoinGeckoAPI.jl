de95de90f2fc8bb81df0889fb4a793846fed75fe
Documenter.jl <documenter@juliadocs.github.io>
2021-12-21 20:29:20 +0000

    build based on 69c9cbd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CoinGeckoAPI","category":"page"},{"location":"#CoinGeckoAPI","page":"Home","title":"CoinGeckoAPI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CoinGeckoAPI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CoinGeckoAPI]","category":"page"}]
+}
