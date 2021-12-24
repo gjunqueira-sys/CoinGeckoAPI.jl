1cc6cb4a37e61fb91cee7a16992a7c8a880f48ea
Gil Junqueira <gjunqueira@msn.com>
2021-12-24 16:51:57 -0600

    started writing a few tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,22 @@ using CoinGeckoAPI
 using Test
 
 
-r = ping()
+r = ping();
 
-r = get_price("bitcoin", "usd")
+r = get_price("bitcoin", "usd");
 
 @testset "CoinGeckoAPI.jl" begin
     # Write your tests here.
+
+    @test 1 ==  1;
+
+    @test ping()["gecko_says"] == "(V3) To the Moon!"
+
+    @test get_price("bitcoin", "usd")["bitcoin"][:usd] |> typeof == Int64 ; # check if price is a number (whatever that is)
+
+    
+
+
+
+
 end
